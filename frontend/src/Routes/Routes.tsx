a0c5f06369758c8e54b3bd3adf747069e0bfb516
Jacqueline <jcasalia@yahoo.com.br>
2025-02-24 10:29:31 -0300

    iniciando o projeto

---
+++ frontend/src/Routes/Routes.tsx
@@ -0,0 +1,33 @@
+
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import React from "react";
+
+
+import Home from "../pages/Home";
+
+
+
+
+
+const AppRoutes = () => {
+ 
+  return (
+    <Router>
+ 
+
+    <Routes>
+      <Route path="/" element={<Home />}/>
+
+     
+
+   
+   
+  
+    </Routes>
+
+  </Router>
+  );
+};
+export default AppRoutes;
