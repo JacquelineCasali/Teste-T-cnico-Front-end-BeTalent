7baf21fa4e44e19899463c01304b3cc1df190966
Jacqueline <jcasalia@yahoo.com.br>
2025-02-26 08:50:14 -0300

    navbar

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+
+
+import "./navbar.css"
+import logo from "../../assets/Logo.svg";
+
+export default function Navbar() {
+   
+ return (
+<nav className="nav">
+
+  <img src={logo} />
+      
+
+    </nav>
+  )
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,12 @@
+import Navbar from "../components/Navbar/Navbar";
 
 
 
 export default function Home() {
   return (
-<div className="main">ola</div>
+<div className="main">
+  <Navbar/>
+</div>
   )
  
   
